2e64687ba8f40a496ba5ebc7d0f01541f3933057
Cristian Arellano <zaidarellano@protonmail.com>
2025-11-24 07:41:15 -0500

    feat: update project structure and render setup

--- src/app.js
+++ src/app.js
@@ -1,12 +1,21 @@
-require("dotenv").config({ path: "/home/mackroph/Platzi/Expres/.env" });
+require("dotenv").config({ path: ".env" });
 
 const express = require("express");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static("src/public"));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views")); // ruta a tus plantillas
+
+app.use(express.static(path.join(__dirname, "public"))); // si tienes assets
 
 app.get("/", (_req, _res) => {
+  const data = {
+    port: PORT,
+    message: "Welcome to the Express server",
+  };
+  return _res.render("index", data); // renderiza views/index.ejs
 });
 
 app.listen(PORT, () => {
